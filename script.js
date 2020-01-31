453c18a683432e6a343fced08f61a31c58a698be
Andes0 <41841850+Andes0@users.noreply.github.com>
2020-01-31 08:46:41 -0500

    Peer Review Results

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ document.querySelector(".search-container").appendChild(but);
 document.body.appendChild(modald);
 
 input.addEventListener("keyup", ()=>{
-    console.log(people)
     populate(people, input.value);
 })
 
@@ -32,7 +31,7 @@ function checkStatus(response){
     }
 }
 
-fetchData('https://randomuser.me/api/?results=12')
+fetchData('https://randomuser.me/api/?nat=us&results=12')
     .then(data => {
         people = data.results
         populate(people, "");
@@ -60,12 +59,60 @@ function populate(data, keyword){
 }
 
 function checkWord(data, keyword){
-    return data.name.first.indexOf(keyword) >= 0 || data.name.last.indexOf(keyword) >= 0 || data.email.indexOf(keyword) >= 0 || data.location.city.indexOf(keyword) >= 0 || data.location.state.indexOf(keyword) >= 0;
+    return data.name.first.toLowerCase().indexOf(keyword.toLowerCase()) >= 0 || data.name.last.toLowerCase().indexOf(keyword.toLowerCase()) >= 0 || data.email.toLowerCase().indexOf(keyword.toLowerCase()) >= 0 || data.location.city.toLowerCase().indexOf(keyword.toLowerCase()) >= 0 || data.location.state.toLowerCase().indexOf(keyword.toLowerCase()) >= 0;
 }
 
 function addListeners(data){
-    for(let i = 0; i < 12; i++){
+    for(let i = 0; i < document.getElementsByClassName("card").length; i++){
+
         document.getElementsByClassName("card")[i].addEventListener("click", ()=>{
+
+            let name = document.getElementsByClassName("card")[i].children[1].children[0].innerHTML;
+            let point;
+            for(let i = 0; i < data.length; i++){
+                if(data[i].name.first + " " + data[i].name.last ===  name){
+                    point = data[i];
+                }
+            }
+            document.body.innerHTML += `
+            <div class="modal-container">
+                <div class="modal">
+                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                    <div class="modal-info-container">
+                        <img class="modal-img" src="${point.picture.large}" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">${point.name.first} ${point.name.last}</h3>
+                        <p class="modal-text">${point.email}</p>
+                        <p class="modal-text cap">${point.location.city}</p>
+                        <hr>
+                        <p class="modal-text">${point.phone}, ${point.cell}</p>
+                        <p class="modal-text">${point.location.street.number} ${point.location.street.name} ${point.location.city}, ${point.location.state} ${point.location.postcode}</p>
+                        <p class="modal-text">Birthday: ${point.dob.date.substring(5,7)}/${point.dob.date.substring(8,10)}/${point.dob.date.substring(0,4)}</p>
+                    </div>
+                </div>
+
+                <div class="modal-btn-container">
+                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                </div>
+            </div>`;
+
+            addButtons(data, point);
+        })
+    } 
+}
+
+function addButtons(data, point){
+    let i;
+    for(let j = 0; j < data.length; j++){
+        if(data[j] == point){
+            i = j;
+        }
+    }
+    
+    document.getElementById("modal-prev").addEventListener("click", ()=>{
+        document.body.removeChild(document.querySelector(".modal-container"));
+        if(i != 0){
+            i--;
             document.body.innerHTML += `
             <div class="modal-container">
                 <div class="modal">
@@ -78,7 +125,7 @@ function addListeners(data){
                         <hr>
                         <p class="modal-text">${data[i].phone}, ${data[i].cell}</p>
                         <p class="modal-text">${data[i].location.street.number} ${data[i].location.street.name} ${data[i].location.city}, ${data[i].location.state} ${data[i].location.postcode}</p>
-                        <p class="modal-text">Birthday: ${data[i].dob.substring(5,7)}/${data[i].dob.substring(8,10)}/${data[i].dob.substring(0,4)}10/21/2015</p>
+                        <p class="modal-text">Birthday: ${data[i].dob.date.substring(5,7)}/${data[i].dob.date.substring(8,10)}/${data[i].dob.date.substring(0,4)}</p>
                     </div>
                 </div>
 
@@ -87,7 +134,35 @@ function addListeners(data){
                     <button type="button" id="modal-next" class="modal-next btn">Next</button>
                 </div>
             </div>`;
-        })
-    }
-    
+            addButtons(data, data[i]);
+        }
+    });
+    document.getElementById("modal-next").addEventListener("click", ()=>{
+        document.body.removeChild(document.querySelector(".modal-container"));
+        if(i != data.length -1){
+            i++;
+            document.body.innerHTML += `
+            <div class="modal-container">
+                <div class="modal">
+                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                    <div class="modal-info-container">
+                        <img class="modal-img" src="${data[i].picture.large}" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">${data[i].name.first} ${data[i].name.last}</h3>
+                        <p class="modal-text">${data[i].email}</p>
+                        <p class="modal-text cap">${data[i].location.city}</p>
+                        <hr>
+                        <p class="modal-text">${data[i].phone}, ${data[i].cell}</p>
+                        <p class="modal-text">${data[i].location.street.number} ${data[i].location.street.name} ${data[i].location.city}, ${data[i].location.state} ${data[i].location.postcode}</p>
+                        <p class="modal-text">Birthday: ${data[i].dob.date.substring(5,7)}/${data[i].dob.date.substring(8,10)}/${data[i].dob.date.substring(0,4)}</p>
+                    </div>
+                </div>
+
+                <div class="modal-btn-container">
+                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                </div>
+            </div>`;
+            addButtons(data, data[i]);
+        }
+    });
 }
